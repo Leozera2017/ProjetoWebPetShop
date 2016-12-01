05956035a34d1062d63f022c24283ef817d602ba
Leo Santos <Leo Santos@DESKTOP-JP69VLS>
2016-12-01 09:22:59 -0200

    Criação de PessoaModel

---
+++ ProjetoWebPetShop/Models/PessoaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoWebPetShop.Models
+{
+    public class PessoaModel
+    {
+
+        public string Nome { get; set; }
+        public string NomeEmpresa { get; set; }
+        public string NomeFantasia { get; set; }
+        public string CPFCNPJ { get; set; }
+        public string Telefone1 { get; set; }
+        public string Telefone2 { get; set; }
+    }
+}
